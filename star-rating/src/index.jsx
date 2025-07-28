9fe1bff2e618ee1badc726c2457372843f2d7349
Marianna Mirabile <marianna.mirabile@worldaquatics.com>
2025-07-28 17:05:02 +0200

    Add StarRating with hover and click functionality

--- star-rating/src/index.jsx
+++ star-rating/src/index.jsx
@@ -1,3 +1,50 @@
+import { FaStar } from "react-icons/fa";
+import { useState } from "react";
+import "./styles.css";
+
 export default function StarRating({ numOfStars = 5 }) {
-    return <div className="star-rating">{[...Array(numOfStars)].map()}</div>;
+    const [rating, setRating] = useState(0);
+    const [hover, setHover] = useState(0);
+
+    function handleClick(getCurrentIndex) {
+        // console.log(getCurrentIndex);
+        setRating(getCurrentIndex);
+    }
+
+    function handleMouseEnter(getCurrentIndex) {
+        // console.log(getCurrentIndex);
+        setHover(getCurrentIndex);
+    }
+
+    function handleMouseLeave() {
+        //console.log(getCurrentIndex);
+        setHover(rating);
+    }
+
+    return (
+        <div className="star-rating">
+            {[...Array(numOfStars)].map((_, index) => {
+                index += 1;
+
+                return (
+                    <FaStar
+                        key={index}
+                        className={
+                            index <= (hover || rating) ? "active" : "inactive"
+                        }
+                        onClick={() => {
+                            handleClick(index);
+                        }}
+                        onMouseMove={() => {
+                            handleMouseEnter(index);
+                        }}
+                        onMouseLeave={() => {
+                            handleMouseLeave();
+                        }}
+                        size={30}
+                    />
+                );
+            })}
+        </div>
+    );
 }
