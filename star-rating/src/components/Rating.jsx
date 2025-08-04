2d2d7cf790ba64ac219edc0e6924e6dd27f336c8
Maz <77918037+Maz-hub@users.noreply.github.com>
2025-08-04 17:10:33 +0000

    rating state

--- star-rating/src/components/Rating.jsx
+++ star-rating/src/components/Rating.jsx
@@ -1,24 +1,21 @@
+import { useState } from "react";
+
 const Rating = () => {
+  const [rating, setRating] = useState(0);
+  const [hover, setHover] = useState(0);
+
   const stars = Array.from({ length: 5 }, (_, i) => i + 1);
-
-  const clicked = (index) => {
-    return console.log("Clicked !", index);
-  };
-
-  const hovered = (direction, index) => {
-    return console.log("Hovered", direction, index);
-  };
 
   return (
     <>
       <div className="rating-container">
         <h2>Rate your experience</h2>
         <div className="stars">
-          {stars.map((star, index) => (
+          {stars.map((star) => (
             <span
-              onClick={() => clicked(index)}
-              onMouseEnter={() => hovered("enter", index)}
-              onMouseLeave={() => hovered("leave", index)}
+              onClick={() => setRating(star)}
+              onMouseEnter={() => setHover(star)}
+              onMouseLeave={() => setHover(0)}
               key={star}
               className="star"
             >
