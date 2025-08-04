0b033e42ffd025f5122a4d3f7e9648b6c4f3c3d6
Maz <77918037+Maz-hub@users.noreply.github.com>
2025-08-04 14:39:51 +0000

    added onClick, onMouseEnter and onMouseLeave events

--- star-rating/src/components/Rating.jsx
+++ star-rating/src/components/Rating.jsx
@@ -1,8 +1,33 @@
 const Rating = () => {
+  const stars = Array.from({ length: 5 }, (_, i) => i + 1);
+
+  const clicked = (index) => {
+    return console.log("Clicked !", index);
+  };
+
+  const hovered = (direction, index) => {
+    return console.log("Hovered", direction, index);
+  };
+
   return (
-    <div className="rating-container">
-      <h2>Rate your experience</h2>
-    </div>
+    <>
+      <div className="rating-container">
+        <h2>Rate your experience</h2>
+        <div className="stars">
+          {stars.map((star, index) => (
+            <span
+              onClick={() => clicked(index)}
+              onMouseEnter={() => hovered("enter", index)}
+              onMouseLeave={() => hovered("leave", index)}
+              key={star}
+              className="star"
+            >
+              {"\u2605"}
+            </span>
+          ))}
+        </div>
+      </div>
+    </>
   );
 };
 
