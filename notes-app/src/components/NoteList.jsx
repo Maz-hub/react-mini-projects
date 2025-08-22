a5e5fa94ec36fd8d77e2a922054cc2d97a7c4b94
Maz <77918037+Maz-hub@users.noreply.github.com>
2025-08-22 15:19:20 +0000

    feat(notes): add dynamic border color based on note priority

--- notes-app/src/components/NoteList.jsx
+++ notes-app/src/components/NoteList.jsx
@@ -1,28 +1,43 @@
 const NoteList = ({ notes, deleteNote }) => {
   if (notes.length === 0) {
-    return <p className="text-center text gray-300">No Notes Yet</p>;
+    return <p className="text-center text-gray-300">No Notes Yet</p>;
   }
+
   return (
     <div className="space-y-4">
-      {notes.map((note) => (
-        <div
-          key={note.id}
-          className="p-4 bg-white rounded-lg shadow-md border-l-4"
-        >
-          <h3 className="text-lg font-bold">{note.title}</h3>
-          <p className="text-sm text-gray-600">
-            <strong>Category:</strong> {note.category}
-          </p>
-          <p className="text-sm text-gray-600">
-            <strong>Priority:</strong> {note.priority}
-          </p>
-          <p className="mt-2">{note.description}</p>
+      {notes.map((note) => {
+        let borderColor = "";
+        if (note.priority === "High") {
+          borderColor = "border-red-500";
+        } else if (note.priority === "Medium") {
+          borderColor = "border-yellow-500";
+        } else {
+          borderColor = "border-blue-500";
+        }
 
-          <button onClick={() => deleteNote(note.id)} className="mt-3 text-red-400 cursor-pointer transition hover:text-red-900">
-            🗑️ Delete
-          </button>
-        </div>
-      ))}
+        return (
+          <div
+            key={note.id}
+            className={`p-4 bg-white rounded-lg shadow-md border-l-4 ${borderColor}`}
+          >
+            <h3 className="text-lg font-bold">{note.title}</h3>
+            <p className="text-sm text-gray-600">
+              <strong>Category:</strong> {note.category}
+            </p>
+            <p className="text-sm text-gray-600">
+              <strong>Priority:</strong> {note.priority}
+            </p>
+            <p className="mt-2">{note.description}</p>
+
+            <button
+              onClick={() => deleteNote(note.id)}
+              className="mt-3 text-red-400 cursor-pointer transition hover:text-red-900"
+            >
+              🗑️ Delete
+            </button>
+          </div>
+        );
+      })}
     </div>
   );
 };
