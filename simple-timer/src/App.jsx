15df534fcfe466bde25a8dfb7e5dab201aff7e9a
Maz <77918037+Maz-hub@users.noreply.github.com>
2025-09-15 15:22:33 +0000

    feat(timer): move timer logic to separate Timer component

--- simple-timer/src/App.jsx
+++ simple-timer/src/App.jsx
@@ -1,52 +1,10 @@
-import { useState, useRef } from "react";
+import Timer from "./components/Timer.jsx";
 
-function App() {
-  const timerRef = useRef(null);
-  const [time, setTime] = useState(0);
-  const [isRunning, setIsRunning] = useState(false);
-
-  const toggleTimer = () => {
-    // Check to see if it's running ?
-    if (isRunning) {
-      // Clear the interval to stop the timer
-      clearInterval(timerRef.current);
-      timerRef.current = null;
-    } else {
-      // Start the timer
-      timerRef.current = setInterval(() => {
-        setTime((prevTime) => prevTime + 1);
-      }, 1000);
-    }
-
-    setIsRunning(!isRunning);
-  };
-
-  // Reset Timer
-  const resetTimer = () => {
-    clearInterval(timerRef.current); // stop the timer
-    setIsRunning(false);
-    setTime(0);
-    timerRef.current = null; // reset
-  };
+const App = () => {
 
   return (
     <div className="max-w-md mx-auto mt-10 p-6 bg-amber-100 rounded-lg shadow-lg text-center">
-      <h2 className="text-4xl font-semibold mt-4"> Timer: {time}</h2>
-      {/* Start & Pause button */}
-      <button
-        onClick={toggleTimer}
-        className="mt-10 bg-amber-700 text-white px-4 py-2 mr-5 rounded cursor-pointer hover:bg-amber-600"
-      >
-        {isRunning ? "Pause" : "Start"}
-      </button>
-
-      {/* Reset button */}
-      <button
-        onClick={resetTimer}
-        className="mt-10 bg-amber-950 text-white px-4 py-2 rounded cursor-pointer hover:bg-amber-900"
-      >
-        Reset
-      </button>
+      <Timer/>
     </div>
   );
 }
