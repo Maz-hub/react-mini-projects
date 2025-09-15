5f6605776b312094103ce97a4dbf84e5e57f28bf
Maz <77918037+Maz-hub@users.noreply.github.com>
2025-09-15 14:24:55 +0000

    feat(timer): implement toggle functionality for starting and pausing the timer

--- simple-timer/src/App.jsx
+++ simple-timer/src/App.jsx
@@ -5,7 +5,32 @@ function App() {
   const [time, setTime] = useState(0);
   const [isRunning, setIsRunning] = useState(false);
 
-  return <>My App</>;
+  const toggleTimer = () => {
+    // Check to see if it's running ?
+    if (isRunning) {
+      // Clear the interval to stop the timer
+      clearInterval(timerRef.current);
+      timerRef.current = null;
+    } else {
+      // Start the timer
+      timerRef.current = setInterval(() => {
+        setTime((prevTime) => prevTime + 1);
+      }, 1000);
+    }
+  };
+
+  return (
+    <div className="max-w-md mx-auto mt-10 p-6 bg-amber-100 rounded-lg shadow-lg text-center">
+      <h2 className="text-4xl font-semibold mt-4"> Timer: {time}</h2>
+      {/* Start & Pause button */}
+      <button
+        onClick={toggleTimer}
+        className="mt-10 bg-amber-700 text-white px-4 py-2 rounded cursor-pointer hover:bg-amber-600"
+      >
+        {isRunning ? "Pause" : "Start"}
+      </button>
+    </div>
+  );
 }
 
 export default App;
