87be8c97973d7bbf1faf54bb38dcc7748782fc5b
Maz <77918037+Maz-hub@users.noreply.github.com>
2025-09-15 14:32:56 +0000

    feat(timer): add reset functionality to timer

--- simple-timer/src/App.jsx
+++ simple-timer/src/App.jsx
@@ -21,15 +21,31 @@ function App() {
     setIsRunning(!isRunning);
   };
 
+  // Reset Timer
+  const resetTimer = () => {
+    clearInterval(timerRef.current); // stop the timer
+    setIsRunning(false);
+    setTime(0);
+    timerRef.current = null; // reset
+  };
+
   return (
     <div className="max-w-md mx-auto mt-10 p-6 bg-amber-100 rounded-lg shadow-lg text-center">
       <h2 className="text-4xl font-semibold mt-4"> Timer: {time}</h2>
       {/* Start & Pause button */}
       <button
         onClick={toggleTimer}
-        className="mt-10 bg-amber-700 text-white px-4 py-2 rounded cursor-pointer hover:bg-amber-600"
+        className="mt-10 bg-amber-700 text-white px-4 py-2 mr-5 rounded cursor-pointer hover:bg-amber-600"
       >
         {isRunning ? "Pause" : "Start"}
+      </button>
+
+      {/* Reset button */}
+      <button
+        onClick={resetTimer}
+        className="mt-10 bg-amber-950 text-white px-4 py-2 rounded cursor-pointer hover:bg-amber-900"
+      >
+        Reset
       </button>
     </div>
   );
