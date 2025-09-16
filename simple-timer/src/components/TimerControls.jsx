e3ab07bb66e12ce419d0d082410539cca07b5ddc
Maz <77918037+Maz-hub@users.noreply.github.com>
2025-09-16 08:26:43 +0000

    add Auto-Focus Start Button

--- simple-timer/src/components/TimerControls.jsx
+++ simple-timer/src/components/TimerControls.jsx
@@ -1,8 +1,18 @@
-const TimerControls = ({isRunning, toggleTimer, resetTimer}) => {
+import { useEffect, useRef } from "react";
+
+const TimerControls = ({ isRunning, toggleTimer, resetTimer }) => {
+  const startButtonRef = useRef(null);
+
+  useEffect(() => {
+    if (startButtonRef.current) {
+      startButtonRef.current.focus();
+    }
+  }, []);
   return (
     <>
       {/* Start & Pause button */}
       <button
+        ref={startButtonRef}
         onClick={toggleTimer}
         className="mt-10 bg-amber-700 text-white px-4 py-2 mr-5 rounded cursor-pointer hover:bg-amber-600"
       >
